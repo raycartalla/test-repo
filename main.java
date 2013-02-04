b54f6353c715068a2d9799d7222902ec457f1dfe
Ray Cartalla <raycartalla@yahoo.com>
2013-02-04 16:13:26 +1100

    supplying a variable
    added a new variable
    v.1.0

--- main.java
+++ main.java
@@ -6,8 +6,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x;
+		
 		System.out.println("Hello World!!");
+		
+		System.out.println("The value is %d",x);
 	}
 
 }
