02f8dd86aa3359c7ffcd01cbf38025476e602392
Ray Cartalla <raycartalla@yahoo.com>
2013-02-04 16:16:58 +1100

    created a variable
    new variable y is added
    v1.1

--- main.java
+++ main.java
@@ -7,10 +7,12 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		int x;
+		int y;
 		
 		System.out.println("Hello World!!");
 		
 		System.out.println("The value is %d",x);
+		System.out.println("The new attribute for y is %d",y);
 	}
 
 }
